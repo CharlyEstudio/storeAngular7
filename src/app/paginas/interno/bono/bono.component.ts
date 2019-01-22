109809bd6e56f4249def2c43a4550029488d5acc
CharlyRamirez <pingestudio@gmail.com>
2019-01-22 11:04:36 -0600

    sección de bonos terminado

--- src/app/paginas/interno/bono/bono.component.ts
+++ src/app/paginas/interno/bono/bono.component.ts
@@ -15,52 +15,52 @@ export class BonoComponent implements OnInit {
     {data: [0, 0, 0, 0], label: 'Mes 3'}
   ];
 
+  mes1: any;
+  cantMes1: number = 0;
+  promMes1: number = 0;
+  mes2: any;
+  cantMes2: number = 0;
+  promMes2: number = 0;
+  mes3: any;
+  cantMes3: number = 0;
+  promMes3: number = 0;
+
+  pasaCompras: boolean = false;
+  pasaComision: boolean = false;
+
   lineChartLabels: Array<any> = ['Semana 1', 'Semana 2', 'Semana 3', 'Semana 4'];
   lineChartOptions: any = {
     responsive: true
   };
 
   lineChartColors: Array<any> = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    { // rojo
+      backgroundColor: 'rgba(206, 52, 76,0.2)', // fondo
+      borderColor: 'rgba(206, 52, 76,1)', // puntos
+      pointBackgroundColor: 'rgba(206, 52, 76,1)',
+      pointBorderColor: '#fff', // bordes
+      pointHoverBackgroundColor: '#fff', // bordes
+      pointHoverBorderColor: 'rgba(206, 52, 76,0.8)' // línea
     },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
+    { // azul
+      backgroundColor: 'rgba(52, 136, 206,0.2)',
+      borderColor: 'rgba(52, 136, 206,1)',
+      pointBackgroundColor: 'rgba(52, 136, 206,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
+      pointHoverBorderColor: 'rgba(52, 136, 206,1)'
     },
     { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
+      backgroundColor: 'rgba(101, 106, 102,0.2)',
+      borderColor: 'rgba(101, 106, 102,1)',
+      pointBackgroundColor: 'rgba(101, 106, 102,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+      pointHoverBorderColor: 'rgba(101, 106, 102,0.8)'
     }
   ];
 
-  // fecMes1Sem1: any;
-  // fecMes1Sem2: any;
-  // fecMes1Sem3: any;
-  // fecMes1Sem4: any;
-
-  // fecMes2Sem1: any;
-  // fecMes2Sem2: any;
-  // fecMes2Sem3: any;
-  // fecMes2Sem4: any;
-
-  // fecMes3Sem1: any;
-  // fecMes3Sem2: any;
-  // fecMes3Sem3: any;
-  // fecMes3Sem4: any;
+  fec: number = Date.now();
 
   fecha: string;
   mes: number;
@@ -111,6 +111,9 @@ export class BonoComponent implements OnInit {
     this.numero = this._usuarioService.usuario.numero;
     this.idFerrum = Number(this._usuarioService.usuario.idFerrum);
     this.trimestreBono = JSON.stringify(this._datoService.trimestreBono(this.mes, this.year));
+    this.mes1 = this._datoService.trimestreBono(this.mes, this.year).mes1;
+    this.mes2 = this._datoService.trimestreBono(this.mes, this.year).mes2;
+    this.mes3 = this._datoService.trimestreBono(this.mes, this.year).mes3;
   }
 
   ngOnInit() {
@@ -144,7 +147,6 @@ export class BonoComponent implements OnInit {
     });
 
     this._datoService.obtenerComprasTrimestrales(this._usuarioService.usuario.numero, this.trimestreBono).subscribe((compras: any) => {
-      console.log(compras);
       let fecMes1Sem1;
       let fecMes1Sem2;
       let fecMes1Sem3;
@@ -163,36 +165,111 @@ export class BonoComponent implements OnInit {
       if (compras.status) {
         for (let i = 0; i < compras.respuesta.length; i++) {
           if (compras.respuesta[i].mes === 'MES1') {
-            fecMes1Sem1 = Number(compras.respuesta[i].cantidad);
-            fecMes1Sem2 = Number(compras.respuesta[i].cantidad);
-            fecMes1Sem3 = Number(compras.respuesta[i].cantidad);
-            fecMes1Sem4 = Number(compras.respuesta[i].cantidad);
-            console.log(compras.respuesta[i]);
+            if (compras.respuesta[i].semana === 'SEMANA1') {
+              fecMes1Sem1 = Number(compras.respuesta[i].cantidad);
+            } else if (compras.respuesta[i].semana === 'SEMANA2') {
+              fecMes1Sem2 = Number(compras.respuesta[i].cantidad);
+            } else if (compras.respuesta[i].semana === 'SEMANA3') {
+              fecMes1Sem3 = Number(compras.respuesta[i].cantidad);
+            } else if (compras.respuesta[i].semana === 'SEMANA4') {
+              fecMes1Sem4 = Number(compras.respuesta[i].cantidad);
+            }
+            this.cantMes1 += Number(compras.respuesta[i].cantidad);
           }
 
           if (compras.respuesta[i].mes === 'MES2') {
-            fecMes2Sem1 = Number(compras.respuesta[i].cantidad);
-            fecMes2Sem2 = Number(compras.respuesta[i].cantidad);
-            fecMes2Sem3 = Number(compras.respuesta[i].cantidad);
-            fecMes2Sem4 = Number(compras.respuesta[i].cantidad);
-            console.log(compras.respuesta[i]);
+            if (compras.respuesta[i].semana === 'SEMANA1') {
+              fecMes2Sem1 = Number(compras.respuesta[i].cantidad);
+            } else if (compras.respuesta[i].semana === 'SEMANA2') {
+              fecMes2Sem2 = Number(compras.respuesta[i].cantidad);
+            } else if (compras.respuesta[i].semana === 'SEMANA3') {
+              fecMes2Sem3 = Number(compras.respuesta[i].cantidad);
+            } else if (compras.respuesta[i].semana === 'SEMANA4') {
+              fecMes2Sem4 = Number(compras.respuesta[i].cantidad);
+            }
+            this.cantMes2 += Number(compras.respuesta[i].cantidad);
           }
 
           if (compras.respuesta[i].mes === 'MES3') {
-            fecMes3Sem1 = Number(compras.respuesta[i].cantidad);
-            fecMes3Sem2 = Number(compras.respuesta[i].cantidad);
-            fecMes3Sem3 = Number(compras.respuesta[i].cantidad);
-            fecMes3Sem4 = Number(compras.respuesta[i].cantidad);
-            console.log(compras.respuesta[i]);
+            if (compras.respuesta[i].semana === 'SEMANA1') {
+              fecMes3Sem1 = Number(compras.respuesta[i].cantidad);
+            } else if (compras.respuesta[i].semana === 'SEMANA2') {
+              fecMes3Sem2 = Number(compras.respuesta[i].cantidad);
+            } else if (compras.respuesta[i].semana === 'SEMANA3') {
+              fecMes3Sem3 = Number(compras.respuesta[i].cantidad);
+            } else if (compras.respuesta[i].semana === 'SEMANA4') {
+              fecMes3Sem4 = Number(compras.respuesta[i].cantidad);
+            }
+            this.cantMes3 += Number(compras.respuesta[i].cantidad);
           }
         }
 
+        if (fecMes1Sem1 > 0) {
+          this.promMes1 += 1;
+        }
+
+        if (fecMes1Sem2 > 0) {
+          this.promMes1 += 1;
+        }
+
+        if (fecMes1Sem3 > 0) {
+          this.promMes1 += 1;
+        }
+
+        if (fecMes1Sem4 > 0) {
+          this.promMes1 += 1;
+        }
+
+        if (fecMes2Sem1 > 0) {
+          this.promMes2 += 1;
+        }
+
+        if (fecMes2Sem2 > 0) {
+          this.promMes2 += 1;
+        }
+
+        if (fecMes2Sem3 > 0) {
+          this.promMes2 += 1;
+        }
+
+        if (fecMes2Sem4 > 0) {
+          this.promMes2 += 1;
+        }
+
+        if (fecMes3Sem1 > 0) {
+          this.promMes3 += 1;
+        }
+
+        if (fecMes3Sem2 > 0) {
+          this.promMes3 += 1;
+        }
+
+        if (fecMes3Sem3 > 0) {
+          this.promMes3 += 1;
+        }
+
+        if (fecMes3Sem4 > 0) {
+          this.promMes3 += 1;
+        }
+
+        if (2 >= this.promMes1 && 2 >= this.promMes2 && 2 >= this.promMes3) {
+          this.pasaCompras = true;
+        } else {
+          this.pasaCompras = false;
+        }
+
         this.lineChartData = [
-          {data: [fecMes1Sem1, fecMes1Sem2, fecMes1Sem3, fecMes1Sem4], label: 'Mes 1'},
-          {data: [fecMes2Sem1, fecMes2Sem2, fecMes2Sem3, fecMes2Sem4], label: 'Mes 2'},
-          {data: [fecMes3Sem1, fecMes3Sem2, fecMes3Sem3, fecMes3Sem4], label: 'Mes 3'}
+          {data: [fecMes1Sem1, fecMes1Sem2, fecMes1Sem3, fecMes1Sem4], label: this.mes1},
+          {data: [fecMes2Sem1, fecMes2Sem2, fecMes2Sem3, fecMes2Sem4], label: this.mes2},
+          {data: [fecMes3Sem1, fecMes3Sem2, fecMes3Sem3, fecMes3Sem4], label: this.mes3}
         ];
-        console.log(this.lineChartData);
+
+        if (this.compras >= 20000 && this.numVenc === 0 && this.pasaCompras) {
+          this.pasaComision = true;
+        } else {
+          this.pasaComision = false;
+        }
+
       }
     });
   }
