e59a9094773ec9d9b514184acb37e4213805e971
CharlyEstudio <pingestudio@gmail.com>
2019-11-13 15:55:01 -0600

    Arreglo en Facturas

--- src/app/servicios/pipes/pdf/pdf.pipe.ts
+++ src/app/servicios/pipes/pdf/pdf.pipe.ts
@@ -3,14 +3,19 @@ import { DomSanitizer } from '@angular/platform-browser';
 
 // Links
 import { LINK } from 'src/app/config/config';
-import { componentNeedsResolution } from '@angular/core/src/metadata/resource_loading';
+
+// Servicios
+import { UsuarioServicesService } from 'src/app/servicios/servicios.index';
 
 @Pipe({
   name: 'pdf'
 })
 export class PdfPipe implements PipeTransform {
+  token: any = '';
+
   constructor(
-    public sanitizer: DomSanitizer
+    public sanitizer: DomSanitizer,
+    private usuarioServicesService: UsuarioServicesService
   ) {}
 
   transform(pdf: string, tipo: string = 'pdf'): any {
@@ -27,17 +32,19 @@ export class PdfPipe implements PipeTransform {
     }
 
     let file;
+    this.token = this.usuarioServicesService.token;
 
     switch ( tipo ) {
       case 'pdf':
-        url += '/' + pdf;
+        url += '/' + pdf + '?token=' + this.token;
 
         file = this.sanitizer.bypassSecurityTrustResourceUrl(url);
         break;
-      case 'xml':
-        url += '/' + pdf;
+        case 'xml':
+          url += '/xml/' + pdf + '?token=' + this.token;
 
-        file = url;
+          // file = url;
+          file = this.sanitizer.bypassSecurityTrustResourceUrl(url);
         break;
       default:
         console.log('Tipo de formato no existe');
